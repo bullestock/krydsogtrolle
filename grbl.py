b97ed637a816eb182e236ce54df09fd117e179b6
Torsten Martinsen <torsten@bullestock.net>
2022-09-11 11:32:12 +0200

    Add more methods.

--- grbl.py
+++ grbl.py
@@ -15,6 +15,8 @@ class Grbl:
         self.ser.write(b"$H\n")
         time.sleep(0.5)
         self.wait_for_ok()
+        self.pen_up_position = 200
+        self.pen_down_position = 700
 
     def wait_for_ok(self):
         while True:
@@ -38,10 +40,38 @@ class Grbl:
                     break
             time.sleep(0.2)
 
+    def pen(self, up):
+        print("Pen %s" % ('up' if up else 'down'))
+        pos = self.pen_up_position if up else self.pen_down_position
+        self.ser.write(b"M3S%d\n" % pos)
+        self.wait_for_ok()
+        time.sleep(0.2)
+
+    def line(self, x1, y1, x2, y2, speed=10000):
+        self.goto(x1, y1, speed=speed)
+        self.pen(False)
+        self.goto(x2, y2, speed=speed)
+
+    def grid(self, xo, yo, dist, speed=10000):
+        l.line(xo + 1*dist, yo,
+               xo + 1*dist, yo + 3*dist, speed=speed)
+        l.pen(True)
+        l.line(xo + 2*dist, yo + 3*dist,
+               xo + 2*dist, yo, speed=speed)
+        l.pen(True)
+        l.line(xo, yo + 1*dist,
+               xo + 3*dist, xo + 1*dist, speed=speed)
+        l.pen(True)
+        l.line(xo + 3*dist, xo + 2*dist,
+               xo, yo + 2*dist, speed=speed)
+
 if __name__ == "__main__":
     l = Grbl()
-    l.goto(10, 0)
-    l.goto(0, 50, 3000)
-    l.goto(50, 50)
-    l.goto(0, 0)
-    
+    if False:
+        l.goto(10, 0)
+        l.goto(0, 50, 3000)
+        l.pen(False)
+        l.goto(50, 50)
+        l.pen(True)
+        l.goto(0, 0)
+    l.grid(80, 80, 40, speed=15000)
