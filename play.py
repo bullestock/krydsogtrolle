27de0785f8768831132a02ed183fdcef7bbeaf60
Torsten Martinsen <torsten@bullestock.net>
2022-10-22 20:51:12 +0200

    Use a4detect to determine paper boundaries.

--- play.py
+++ play.py
@@ -2,6 +2,7 @@ import argparse
 import cv2
 import grbl
 import numpy
+import subprocess
 from shapely.geometry import LineString, Point
 
 port = 0
@@ -27,35 +28,38 @@ def get_frame():
 def detect_paper_boundaries():
     frame = get_frame()
     cv2.imwrite('paper.png', frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    blur = cv2.medianBlur(gray, 5)
-    ret, bin_img = cv2.threshold(blur, 128, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    print("Threshold: %d" % ret)
-    cv2.imwrite('binpaper.png', bin_img)
-    contours, hierarchy = cv2.findContours(bin_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    im2 = frame
-    cv2.drawContours(im2, contours, -1, (255, 0, 0), 2)
-    cv2.imwrite('paperlines1.png', im2)
-    paper_contours = []
-    for c in contours:
-        if len(c) > 3 and len(c) < 5:
-            min_x = 9999
-            max_x = 0
-            for e in c:
-                min_x = min(min_x, e[0][0])
-                max_x = max(max_x, e[0][0])
-            dx = max_x - min_x
-            if dx > 100:
-                print("c %s" % c)
-                paper_contours.append(c)
-            else:
-                print("skip")
-    im3 = frame
-    cv2.drawContours(im3, paper_contours, -1, (0, 0, 255), 1)
-    cv2.imwrite('paperimportantlines.png', im3)
-    print(len(contours))
-    print(len(paper_contours))
-    return ((575, 310), (1930, 1245))
+    out = subprocess.run(["./a4detect", 'paper.png'], capture_output=True)
+    assert out.returncode == 0, 'a4detect failed'
+    strings = out.stdout.decode().splitlines()
+    #print(strings)
+    coords = []
+    for s in strings:
+        words = s.split(',')
+        bounds = [int(x) for x in words]
+        coords.append([bounds[0], bounds[1]])
+        coords.append([bounds[2], bounds[3]])
+    #print(coords)
+    xsum = 0
+    ysum = 0
+    for c in coords:
+        xsum = xsum + c[0]
+        ysum = ysum + c[1]
+    xavg = xsum/len(coords)
+    yavg = ysum/len(coords)
+    xmin = 99999
+    xmax = 0
+    ymin = 99999
+    ymax = 0
+    for c in coords:
+        if c[0] > xavg:
+            xmax = max(xmax, c[0])
+        else:
+            xmin = min(xmin, c[0])
+        if c[1] > yavg:
+            ymax = max(ymax, c[1])
+        else:
+            ymin = min(ymin, c[1])
+    return ((xmin, ymin), (xmax, ymax))
 
 # Compute pen boundaries in GRBL coordinates
 def compute_pen_boundaries():
@@ -111,7 +115,6 @@ if not args.noplotter:
     plotter.goto(grbl.Grbl.MAX_X, grbl.Grbl.MAX_Y)
 
 paper = detect_paper_boundaries()
-exit()
 pen = compute_pen_boundaries()
 active_square = find_next_free_square()
 active_square_origin = (pen[0][0] + active_square[0] * square_size,
