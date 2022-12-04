ab5bca322cb72b0e879a5193e57ed5d9e43177e4
Torsten Martinsen <torsten@bullestock.net>
2022-12-04 21:46:20 +0100

    Use 10 x 2 magnets.

--- camera/paperholder.py
+++ camera/paperholder.py
@@ -5,15 +5,17 @@ fillet_r = 1.5
 
 centerXY = (True, True, False)
 
-height = 7
+height = 3
+iw = 85
 width = 92
-thickness = 9
-mcc = width - thickness - 2
+thickness = 12
+ow = iw + 2*thickness
+mcc = (iw + ow)/2 - 2
 result = (cq.Workplane("XY")
           .tag("o")
-          .box(width, width, height)
+          .box(ow, ow, height)
           .workplaneFromTagged("o")
-          .rect(width - 2*thickness, width - 2*thickness)
+          .rect(iw, iw)
           .cutThruAll()
           .edges("|Z")
           .fillet(2)
@@ -22,7 +24,7 @@ result = (cq.Workplane("XY")
           .workplaneFromTagged("o")
           .transformed(offset=(0, 0, -height/2+1))
           .rarray(mcc, mcc, 2, 2)
-          .circle(6.5/2)
+          .circle(10.1/2)
           .cutBlind(10)
           )
 show_object(result)
