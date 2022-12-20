fb8baa235c78163a79f0827f00e96757ced11177
Torsten Martinsen <torsten@bullestock.net>
2022-12-20 21:34:05 +0100

    Tweak.

--- camera/paperholder.py
+++ camera/paperholder.py
@@ -6,7 +6,7 @@ fillet_r = 1.5
 centerXY = (True, True, False)
 
 height = 3
-iw = 85
+iw = 80
 width = 92
 thickness = 12
 ow = iw + 2*thickness
@@ -22,9 +22,9 @@ result = (cq.Workplane("XY")
           .faces(">Z")
           .fillet(1)
           .workplaneFromTagged("o")
-          .transformed(offset=(0, 0, -height/2+1))
+          .transformed(offset=(0, 0, -height/2+0.12))
           .rarray(mcc, mcc, 2, 2)
-          .circle(10.1/2)
+          .circle(10.2/2)
           .cutBlind(10)
           )
 show_object(result)
